9cf3bfffad5566792dd62b43c5dfbb0c357c57df
Paul Shapiro <pshapiro138@gmail.com>
2016-10-29 21:39:53 -0400

    built document_cryptor and added tests - went with in-memory/application layer decryption instead of db-layer even though the latter would probably have given us querying on encrypted data for free because the former allows us to keep the implementation-agnostic Persistor protocol in the rest of the application code so it can be used across platforms w/o each platform persistence system having to build its own per-column encryption

--- local_modules/symmetric_cryptor/document_cryptor.js
+++ local_modules/symmetric_cryptor/document_cryptor.js
@@ -1,1 +1,110 @@
+"use strict"
+//
+//
 var symmetric_string_cryptor = require('./symmetric_string_cryptor')
+//
+//
+// Public constants:
+//
+var CryptSchemeFieldValueTypes =
+{
+	String: "String",
+	Array: "Array",
+	JSON: "JSON"
+}
+exports.CryptSchemeFieldValueTypes = CryptSchemeFieldValueTypes
+//
+//
+// Internal constants:
+//
+var __wrappedDataValueRootKey = 
+{
+	Array: "__array"
+}
+//
+//
+// These functions return shallow copies of all but en/decrypted key values
+//
+function New_EncryptedDocument(plaintextDocument, documentCryptScheme, password)
+{
+	var encryptedDocument = {}
+	//	
+	const plaintextDocument_keys = Object.keys(plaintextDocument)
+	for (let documentKey of plaintextDocument_keys) {
+		let documentCryptScheme_forKey = documentCryptScheme[documentKey]
+		var plaintextValue = plaintextDocument[documentKey]
+		var finalizedValue // to derive
+		if (typeof documentCryptScheme_forKey !== 'undefined') {
+			var keyValue_plaintextType = documentCryptScheme_forKey.type
+			var finalizedPlaintextStringToEncrypt // to derive 
+			// TODO: assert type not nil here?
+			if (keyValue_plaintextType === "String") {
+				finalizedPlaintextStringToEncrypt = plaintextValue
+			} else if (keyValue_plaintextType === "Array") {
+				// wrap in dictionary at __array and patch to JSON codepath
+				var wrappedArray = {}
+				wrappedArray[__wrappedDataValueRootKey.Array] = plaintextValue
+				// store as JSON:
+				finalizedPlaintextStringToEncrypt = JSON.stringify(wrappedArray)
+			} else if (keyValue_plaintextType === "JSON") {
+				finalizedPlaintextStringToEncrypt = JSON.stringify(plaintextValue)
+			} else {
+				// TODO: assert false/fatalerror
+				console.log("Error: Unrecognized document_cryptor key value type", keyValue_plaintextType)
+				process.exit(1)
+			}
+			finalizedValue = symmetric_string_cryptor.EncryptedBase64String(
+				finalizedPlaintextStringToEncrypt, 
+				password
+			)
+		} else {
+			finalizedValue = plaintextValue // just use plaintext
+		}
+		//
+		encryptedDocument[documentKey] = finalizedValue
+	}
+	//
+	return encryptedDocument
+}
+exports.New_EncryptedDocument = New_EncryptedDocument
+//
+function New_DecryptedDocument(encryptedDocument, documentCryptScheme, password)
+{
+	var decryptedDocument = {}
+	//	
+	const encryptedDocument_keys = Object.keys(encryptedDocument)
+	for (let documentKey of encryptedDocument_keys) {
+		let documentCryptScheme_forKey = documentCryptScheme[documentKey]
+		var potentiallyEncryptedValue = encryptedDocument[documentKey]
+		var finalizedValue // to derive
+		if (typeof documentCryptScheme_forKey !== 'undefined') {
+			var keyValue_plaintextType = documentCryptScheme_forKey.type
+			var decryptedStoredValue = symmetric_string_cryptor.DecryptedPlaintextString(
+				potentiallyEncryptedValue, 
+				password
+			)
+			// TODO: check null/undefined here?
+			// TODO: assert type not nil here?
+			if (keyValue_plaintextType === "String") {
+				finalizedValue = decryptedStoredValue
+			} else if (keyValue_plaintextType === "Array") {
+				// wrap in dictionary at __array and patch to JSON codepath
+				var json = JSON.parse(decryptedStoredValue)
+				finalizedValue = json[__wrappedDataValueRootKey.Array]
+			} else if (keyValue_plaintextType === "JSON") {
+				finalizedValue = JSON.parse(decryptedStoredValue)
+			} else {
+				// TODO: assert false/fatalerror
+				console.log("Error: Unrecognized document_cryptor key value type", keyValue_plaintextType)
+				process.exit(1)
+			}
+		} else {
+			finalizedValue = potentiallyEncryptedValue // just use potentiallyEncryptedValue as it should be plaintext
+		}
+		//
+		decryptedDocument[documentKey] = finalizedValue
+	}
+	//
+	return decryptedDocument
+}
+exports.New_DecryptedDocument = New_DecryptedDocument
--- local_modules/symmetric_cryptor/tests/tests.js
+++ local_modules/symmetric_cryptor/tests/tests.js
@@ -5,7 +5,7 @@ const async = require('async')
 async.series(
 	[
 		_proceedTo_test_stringCryptor,
-		// _proceedTo_test_documentCryptor
+		_proceedTo_test_documentCryptor
 	],
 	function(err)
 	{
@@ -20,8 +20,6 @@ async.series(
 //
 function _proceedTo_test_stringCryptor(fn)
 {
-	console.log(Function)
-	//
 	const symmetric_string_cryptor = require('../symmetric_string_cryptor')
 	//
 	const password = "super secret words!"
@@ -50,7 +48,7 @@ function _proceedTo_test_stringCryptor(fn)
 	function __proceedTo_test_string_decryption(fn)
 	{
 		if (encryptedString === null) {
-			var errStr = "Cannot decrypt as encrypt didn't succeed"
+			var errStr = "Cannot decrypt message as encrypt didn't succeed"
 			var err = new Error(errStr)
 			console.log(errStr)
 			//
@@ -64,6 +62,77 @@ function _proceedTo_test_stringCryptor(fn)
 			fn()
 		} else {
 			var errStr = "Error: Test failed. Decrypted message did not match original plaintext message."
+			var err = new Error(errStr)
+			console.log(errStr)
+			//
+			fn(err)
+		}
+	}
+}
+//
+function _proceedTo_test_documentCryptor(fn)
+{
+	const document_cryptor = require('../document_cryptor')
+	//
+	const password = "super secret words!"
+	const plaintextDocument =
+	{
+		"_id": 1,
+		"plaintext_msg": "a plaintext message",
+		"secret_msg": "here's a secret message!",
+		"secrets": [
+			{ "_id": 1, "msg": "abc" },
+			{ "_id": 2, "msg": "def" }
+		],
+		"secretMsgs_by_id": {
+			"1": "abc",
+			"2": "def"
+		}
+	}
+	var documentCryptScheme =
+	{
+		"secret_msg": { type: "String" },
+		"secrets": { type: "Array" },
+		"secretMsgs_by_id": { type: "JSON" }
+	}
+	var encryptedDocument = null
+	//
+	async.series(
+		[
+			__proceedTo_test_string_encryption,
+			__proceedTo_test_string_decryption
+		],
+		function(err)
+		{
+			fn(err)
+		}
+	)
+	//
+	function __proceedTo_test_string_encryption(fn)
+	{
+		encryptedDocument = document_cryptor.New_EncryptedDocument(plaintextDocument, documentCryptScheme, password)
+		// ^ using a parent scope reference like this is kinda brittle - would be nice to pass directly to next test as arg 
+		console.log("Encrypted document to obtain:", JSON.stringify(encryptedDocument, null, '  '))
+		//
+		fn()
+	}
+	function __proceedTo_test_string_decryption(fn)
+	{
+		if (decryptedDocument === null) {
+			var errStr = "Cannot decrypt document as encrypt didn't succeed"
+			var err = new Error(errStr)
+			console.log(errStr)
+			//
+			fn(err)
+			return
+		}
+		var decryptedDocument = document_cryptor.New_DecryptedDocument(encryptedDocument, documentCryptScheme, password)
+		if (JSON.stringify(decryptedDocument) === JSON.stringify(plaintextDocument)) {
+			console.log("Successfully decrypted document to obtain:", decryptedDocument)
+			//
+			fn()
+		} else {
+			var errStr = "Error: Test failed. Decrypted document did not match original plaintext document."
 			var err = new Error(errStr)
 			console.log(errStr)
 			//
