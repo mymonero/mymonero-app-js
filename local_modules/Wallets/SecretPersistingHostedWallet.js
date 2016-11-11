67b350a1ae869df48d7960e8e9c7e73dba9a1e88
Paul Shapiro <pshapiro138@gmail.com>
2016-11-11 17:38:06 -0500

    wallet: IsAccountCatchingUp

--- local_modules/Wallets/SecretPersistingHostedWallet.js
+++ local_modules/Wallets/SecretPersistingHostedWallet.js
@@ -342,6 +342,13 @@ class SecretPersistingHostedWallet
 		//
 		return monero_wallet_utils.TransactionLockedReason(tx, blockchain_height)
 	}
+	//
+    IsAccountCatchingUp = function()
+	{
+		const self = this
+		//
+        return (self.blockchain_height - self.account_scanned_block_height) >= 10
+    }
 	
 	
 	////////////////////////////////////////////////////////////////////////////////
