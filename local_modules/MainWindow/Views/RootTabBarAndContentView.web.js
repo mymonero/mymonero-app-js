4ed5e13ec1acfae30a9fdbb164eb994ad38e8fa0
Paul Shapiro <pshapiro138@gmail.com>
2017-01-27 15:25:14 -0500

    tried using two different claimed solutions to dragging&dropping but they don't solve the dragleave on removeChild during drag - so for now just disabling the auto-select of the Send tab and left a note how we're requiring users to have it selected it to do a drop for the moment

--- local_modules/MainWindow/Views/RootTabBarAndContentView.web.js
+++ local_modules/MainWindow/Views/RootTabBarAndContentView.web.js
@@ -153,30 +153,48 @@ class RootTabBarAndContentView extends LeftSideTabBarAndContentView
 			self.layer.ondragenter = function(e)
 			{
 				e.preventDefault()
+				e.stopPropagation()
 		        numberOfDragsActive++
 				//
 				if (numberOfDragsActive == 1) { // first time since started drag that entered self.layer - becomes 0 on real dragleave
 					if (_isAllowedToPerformDropOps()) {
-						setTimeout(
-							function()
-							{ // we must not manipulate the DOM in dragenter/start because that causes dragleave to fire immediately in Chrome.
-								self.selectTab_sendFunds()
-								//
-								self.sendTabContentView.PopToRootView(true) // in case they're not on root (debated making this not animated)
-								self.sendTabContentView.DismissModalViewsToView(null, true) // null -> to top stack view
-							}
-						)
+						const indexOf_sendTabContentView = self.IndexOfTabBarContentView(self.sendTabContentView)
+						if (indexOf_sendTabContentView === self._currentlySelectedTabBarItemIndex) {
+							// NOTE: we are not currently able to call self.selectTab_sendFunds below, because it causes
+							// some sort of issue where, I'm guessing, when the current tab view is removed, it doesn't
+							// fire its corresponding dragleave event, which means we never end up being able to disable
+							// the drag drop zone cause we never receive the final numberOfDragsActive=0 dragleave. For that
+							// reason we're only allowing a drag op to start when we're already on the Send tab
+							// We might be able to solve this somehow but it didn't seem important enough in early stages -PS on 1/27/17
+							//
+							setTimeout(
+								function()
+								{ // we must not manipulate the DOM in dragenter/start because that causes dragleave to fire immediately in Chrome.
+									// self.selectTab_sendFunds()
+									//
+									self.sendTabContentView.PopToRootView(true) // in case they're not on root (debated making this not animated)
+									self.sendTabContentView.DismissModalViewsToView(null, true) // null -> to top stack view
+									//
+									self.sendTabContentView._proxied_ondragenter(e)
+								}
+							)
+						}
 					} else { // 
 					}
-					self.sendTabContentView._proxied_ondragenter(e)
 				}
 			}
 	        self.layer.ondragleave = self.layer.ondragend = function(e)
 			{
+				e.preventDefault()
+				e.stopPropagation()
+				
 				numberOfDragsActive--
 				//
 				if (numberOfDragsActive == 0) { // back to 0 - actually left self.layer
-					self.sendTabContentView._proxied_ondragleave(e)
+					const indexOf_sendTabContentView = self.IndexOfTabBarContentView(self.sendTabContentView)
+					if (indexOf_sendTabContentView === self._currentlySelectedTabBarItemIndex) {
+						self.sendTabContentView._proxied_ondragleave(e)
+					}
 				}
 				return false
 	        }
@@ -185,7 +203,10 @@ class RootTabBarAndContentView extends LeftSideTabBarAndContentView
 	            e.preventDefault()
 				e.stopPropagation()
 				numberOfDragsActive = 0 // reset just in case ondragleave wasn't properly fired due to some DOM manipulation or on drop. can happen.
-				self.sendTabContentView._proxied_ondrop(e)
+				const indexOf_sendTabContentView = self.IndexOfTabBarContentView(self.sendTabContentView)
+				if (indexOf_sendTabContentView === self._currentlySelectedTabBarItemIndex) {
+					self.sendTabContentView._proxied_ondrop(e)
+				}
 				return false
 			}
 		}
