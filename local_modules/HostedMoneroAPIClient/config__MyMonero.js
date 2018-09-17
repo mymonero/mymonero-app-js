5aa05b9ce7cfa9373d241f7dae09f9c2f2f05fa3
Paul Shapiro <paulshapiro@users.noreply.github.com>
2018-09-17 16:41:14 -0400

    config__MyMonero.js: removed old 'txn service fee' config vals

--- local_modules/HostedMoneroAPIClient/config__MyMonero.js
+++ local_modules/HostedMoneroAPIClient/config__MyMonero.js
@@ -1,7 +1,4 @@
 module.exports = 
 {
-	API__authority: "api.mymonero.com:8443",
-	//
-	HostingServiceFee_depositAddress: "49VNLa9K5ecJo13bwKYt5HCmA8GkgLwpyFjgGKG6qmp8dqoXww8TKPU2PJaLfAAtoZGgtHfJ1nYY8G2YaewycB4f72yFT6u",
-	HostingServiceFee_txFeeRatioOfNetworkFee: 0.5, // Service fee relative to tx fee (0.5 => 50%)
+	API__authority: "api.mymonero.com:8443"
 }
