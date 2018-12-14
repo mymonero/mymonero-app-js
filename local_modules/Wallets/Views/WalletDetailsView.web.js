dc2de5ded4348fdb70458966c53ca3725b82b27e
Paul Shapiro <paulshapiro@users.noreply.github.com>
2018-12-14 10:06:11 -0500

    'Deposit' back to 'Receive'

--- local_modules/Wallets/Views/WalletDetailsView.web.js
+++ local_modules/Wallets/Views/WalletDetailsView.web.js
@@ -354,7 +354,7 @@ class WalletDetailsView extends View
 	{
 		const self = this
 		const buttonView = commonComponents_actionButtons.New_ActionButtonView(
-			"Deposit", 
+			"Receive", 
 			self.context.crossPlatform_appBundledIndexRelativeAssetsRootPath+"Wallets/Resources/actionButton_iconImage__request@3x.png", // relative to index.html
 			// TODO?: borrowing another module's asset. sort of bad
 			false,
