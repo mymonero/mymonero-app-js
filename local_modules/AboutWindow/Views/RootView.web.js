0e33f331348fad44f353a0c9706453c1d42d0f8b
Paul Shapiro <paulshapiro@users.noreply.github.com>
2019-01-05 11:37:45 -0600

    View on GitHub link url changed to latest release

--- local_modules/AboutWindow/Views/RootView.web.js
+++ local_modules/AboutWindow/Views/RootView.web.js
@@ -111,7 +111,7 @@ class RootView extends View
 			layer.style.webkitFontSmoothing = "subpixel-antialiased"
 			layer.style.cursor = "pointer"
 			layer.innerHTML = "View on GitHub"
-			layer.href = "https://www.github.com/mymonero/mymonero-app-js"
+			layer.href = "https://www.github.com/mymonero/mymonero-app-js/releases/latest"
 			layer.addEventListener("mouseenter", function(e) 
 			{
 				layer.style.textDecoration = "underline"
