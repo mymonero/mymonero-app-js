7b821703538d7707e2c5036f21d12c7461aa0b06
Paul Shapiro <paulshapiro@users.noreply.github.com>
2019-01-08 11:23:08 -0600

    updated web wallet tab bar to include funds requests and contacts tabs to direct user to download app

--- local_modules/Contacts/Views/ContactsDownloadAppEmptyScreenView.Lite.web.js
+++ local_modules/Contacts/Views/ContactsDownloadAppEmptyScreenView.Lite.web.js
@@ -28,31 +28,43 @@
 //
 "use strict"
 //
-const RequestTabContentView_Base = require('./RequestTabContentView_Base.web')
+const View = require('../../Views/View.web')
+const commonComponents_emptyScreens = require('../../MMAppUICommonComponents/emptyScreens.web')
 //
-class RequestTabContentView extends RequestTabContentView_Base
+class ContactsDownloadAppEmptyScreenView extends View
 {
 	constructor(options, context)
 	{
 		super(options, context)
-	}
-	setup()
-	{
-		super.setup() // we must call on super
+		//
 		const self = this
-		{ // walletsListView
-			const options = {}
-			const FundsRequestsListView = require('./FundsRequestsListView.web')
-			const view = new FundsRequestsListView(options, self.context)
-			self.fundsRequestsListView = view
-		}
-		{
-			self.SetStackViews(
-				[
-					self.fundsRequestsListView
-				]
-			)
-		}
+		self.layer.style.width = "100%" 
+		self.layer.style.height = "100%"
+		//
+		const view = new View({}, self.context)
+		const layer = view.layer
+		const margin_side = 16
+		const marginTop = 56
+		layer.style.marginTop = `${marginTop}px`
+		layer.style.marginLeft = margin_side + "px"
+		layer.style.width = `calc(100% - ${2 * margin_side}px)`
+		layer.style.height = `calc(100% - ${marginTop}px - 15px)`
+		//
+		const emptyStateMessageContainerView = commonComponents_emptyScreens.New_EmptyStateMessageContainerView(
+			"👇", 
+			"To create Contacts,<br/><a href=\"https://mymonero.com\" target=\"_blank\" style='color: #11bbec; cursor: pointer; -webkit-user-select: none; text-decoration: none;'>download the app</a>.",
+			self.context,
+			0,
+			0
+		)
+		view.addSubview(emptyStateMessageContainerView)
+		self.addSubview(view)
+	}
+	//
+	// Runtime - Accessors - Navigation
+	Navigation_Title()
+	{
+		return "Contacts"
 	}
 }
-module.exports = RequestTabContentView
+module.exports = ContactsDownloadAppEmptyScreenView
---
+++ local_modules/RequestFunds/Views/RequestTabContentView_Base.web.js
@@ -0,0 +1,95 @@
+// Copyright (c) 2014-2019, MyMonero.com
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//	conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//	of conditions and the following disclaimer in the documentation and/or other
+//	materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//	used to endorse or promote products derived from this software without specific
+//	prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+"use strict"
+//
+const StackAndModalNavigationView = require('../../StackNavigation/Views/StackAndModalNavigationView.web')
+//
+class RequestTabContentView extends StackAndModalNavigationView
+{
+	constructor(options, context)
+	{
+		super(options, context)
+	}
+	setup()
+	{
+		super.setup() // we must call on super
+		const self = this
+		// override and add your implementation specific view
+	}
+	//
+	//
+	// Runtime - Accessors - Implementation of TabBarItem protocol - custom tab bar item styling
+	//
+	TabBarItem_layer_customStyle()
+	{
+		const self = this
+		return {}
+	}
+	TabBarItem_icon_customStyle()
+	{
+		const self = this
+		return {
+			backgroundImage: "url("+self.context.crossPlatform_appBundledIndexRelativeAssetsRootPath+"RequestFunds/Resources/icon_tabBar_fundsRequests@3x.png)",
+			backgroundPosition: "center",
+			backgroundRepeat: "no-repeat",
+			backgroundSize: "24px 24px"
+		}
+	}
+	TabBarItem_icon_selected_customStyle()
+	{
+		const self = this
+		return {
+			backgroundImage: "url("+self.context.crossPlatform_appBundledIndexRelativeAssetsRootPath+"RequestFunds/Resources/icon_tabBar_fundsRequests__active@3x.png)",
+			backgroundPosition: "center",
+			backgroundRepeat: "no-repeat",
+			backgroundSize: "24px 24px"
+		}
+	}
+	// interactivity
+	TabBarItem_shallDisable()
+	{
+		const self = this
+		const passwordController = self.context.passwordController
+		if (passwordController.hasUserSavedAPassword !== true) {
+			return true // no existing data - do disable
+		}
+		if (passwordController.HasUserEnteredValidPasswordYet() !== true) { // has data but not unlocked app
+			return true // because the app needs to be unlocked before they can use it
+		}
+		if (passwordController.IsUserChangingPassword() === true) {
+			return true // changing pw - prevent jumping around
+		}
+		const wallets = self.context.walletsListController.records // figure it's ready by this point
+		const numberOf_wallets = wallets.length
+		const walletsExist = numberOf_wallets !== 0
+		const shallDisable = walletsExist == false // no wallets? disable
+		return shallDisable
+	}
+}
+module.exports = RequestTabContentView
