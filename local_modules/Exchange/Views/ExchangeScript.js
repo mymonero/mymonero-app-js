8dc1fb497f251c9e1d01b1133d4c7942d8f5032a
Karl Buys <karl@karlbuys.com>
2020-08-26 14:58:46 +0200

    WIP: now sends between our test wallets

--- local_modules/Exchange/Views/ExchangeScript.js
+++ local_modules/Exchange/Views/ExchangeScript.js
@@ -9,6 +9,7 @@
     let ExchangeFunctions = require('../../Exchange/Javascript/ExchangeFunctions');
     let loaderPage = document.getElementById('loader');
     let order = {};
+    let exchangePage = document.getElementById('exchangePage');
     let orderBtn = document.getElementById("order-button");
     let orderTimer = {};
     let btcAddressInput = document.getElementById("btcAddress");
@@ -16,21 +17,20 @@
     let walletOptions = document.getElementById('wallet-options');
     let exchangeXmrDiv = document.getElementById('exchange-xmr');
 
-    ExchangeFunctions.getRatesAndLimits();
-
-    setTimeout(() => {
-        cmt = "TODO: Refactor this to toggle loader classes after retrieving data from XMR.to, not on 1000 ms";
-        cmt = "TODO: Set default value for XMR to 1 or (max wallet funds - sending fee), and BTC equivalent amount for input according to rates";
-        loaderPage.classList.remove("active");
-        let exchangePage = document.getElementById('exchangePage');
+    ExchangeFunctions.getRatesAndLimits().then(() => {
+        loaderPage.classList.remove('active');
         exchangePage.classList.add("active");
-    }, 1000);
+    });
+
+
 
     btcAddressInput.addEventListener('input', function() {
         console.log(Utils.validateBTCAddress(btcAddressInput.value));
     });
 
     XMRcurrencyInput.addEventListener('keydown', function(event) {
+        if (event.which == 8) 
+        return;
         if ( (event.which !== 110) 
             && (event.which <= 48 || event.which >= 57) 
             && (event.which <= 96 && event.which >= 105) 
@@ -46,32 +46,18 @@
     });
 
     walletSelector.addEventListener('click', function(event) {
-        console.log('wallet selector clicked');
-        console.log(this.id);
-        console.log(event);
-        let test = document.getElementById('wallet-options');
+        let walletElement = document.getElementById('wallet-options');
         let selectedWallet = document.getElementById('selected-wallet');
-        test.classList.add('active');
-        console.log(event.srcElement);
-        console.log(event.srcElement.parentElement);
-        console.log(event.srcElement.className);
-        console.log(event.srcElement.parentElement.className);
+        walletElement.classList.add('active');
         if (event.srcElement.parentElement.className.includes("optionCell")) {
-            console.log('we picked an option');
-            console.log(event.srcElement.parentElement.dataset);
             let dataAttributes = event.srcElement.parentElement.dataset;
-            console.log(dataAttributes);
             selectedWallet.dataset.walletlabel = dataAttributes.walletlabel;
             selectedWallet.dataset.walletBalance = dataAttributes.walletbalance;
             selectedWallet.dataset.swatch = dataAttributes.swatch;
             let walletLabel = document.getElementById('selected-wallet-label'); 
             let walletBalance = document.getElementById('selected-wallet-balance'); 
             let walletIcon = document.getElementById('selected-wallet-icon'); 
-            
-            console.log(walletLabel);
-            console.log(walletBalance);
-            console.log(walletIcon);
-            test.classList.remove('active');
+            walletElement.classList.remove('active');
             walletIcon.style.backgroundImage = `url('../../../assets/img/wallet-${dataAttributes.swatch}@3x.png'`;
             walletLabel.innerText = dataAttributes.walletlabel;
             walletBalance.innerText = dataAttributes.walletbalance;
@@ -81,19 +67,19 @@
         if (event.srcElement.parentElement.className.includes("selectionDisplayCellView")) {
             console.log('we picked selectionDisplayCellView');
             console.log(event.srcElement.parentElement.dataset);
-            test.classList.add('active');
+            walletElement.classList.add('active');
         }
         if (event.srcElement == 'div.hoverable-cell.utility.selectionDisplayCellView') {
-            console.log('test');
-            console.log(event.srcElement.parentElement.dataset);
-        } else if (1 == 1) {
             
-        }
+        } 
     });
 
 
 
     BTCcurrencyInput.addEventListener('keydown', function(event) {
+        if (event.which == 8) 
+            return;
+
         if ( (event.which !== 110) 
             && (event.which <= 48 || event.which >= 57) 
             && (event.which <= 96 && event.which >= 105) 
@@ -112,14 +98,16 @@
         let BTCToReceive = XMRcurrencyInput.value * ExchangeFunctions.currentRates.price;
         if (BTCToReceive.toFixed(8) > ExchangeFunctions.currentRates.upper_limit) {
             let error = document.createElement('div');
+            error.classList.add('message-label');
             error.id = 'xmrexceeded';
-            error.innerHTML = `You cannot exchange more than ${ExchangeFunctions.currentRates.maximum_xmr.toFixed(12)}. Monero`;
+            error.innerHTML = `You cannot exchange more than ${ExchangeFunctions.currentRates.maximum_xmr.toFixed(12)} XMR`;
             validationMessages.appendChild(error);
         }
         if (BTCToReceive.toFixed(8) < ExchangeFunctions.currentRates.lower_limit) {
             let error = document.createElement('div');
+            error.classList.add('message-label');
             error.id = 'xmrtoolow';
-            error.innerHTML = `You cannot exchange less than ${ExchangeFunctions.currentRates.minimum_xmr.toFixed(12)} Monero.`;
+            error.innerHTML = `You cannot exchange less than ${ExchangeFunctions.currentRates.minimum_xmr.toFixed(12)} XMR.`;
             validationMessages.appendChild(error);
         }
         BTCcurrencyInput.value = BTCToReceive.toFixed(8);
@@ -133,13 +121,19 @@
         if (XMRtoReceive.toFixed(12) > ExchangeFunctions.currentRates.upper_limit) {
             let error = document.createElement('div');
             error.id = 'xmrexceeded';
-            error.innerHTML = `You cannot exchange more than ${ExchangeFunctions.currentRates.upper_limit.toFixed(8)} BTC.`;
+            error.classList.add('message-label');
+            console.log(ExchangeFunctions.currentRates);
+            let btc_amount = parseFloat(ExchangeFunctions.currentRates.upper_limit);
+            error.innerHTML = `You cannot exchange more than ${btc_amount} BTC.`;
             validationMessages.appendChild(error);
         }
         if (XMRtoReceive.toFixed(12) < ExchangeFunctions.currentRates.lower_limit) {
             let error = document.createElement('div');
             error.id = 'xmrtoolow';
-            error.innerHTML = `You cannot exchange less than ${ExchangeFunctions.currentRates.lower_limit.toFixed(8)} BTC.`;
+            error.classList.add('message-label');
+            console.log(ExchangeFunctions.currentRates);
+            let btc_amount = parseFloat(ExchangeFunctions.currentRates.lower_limit);
+            error.innerHTML = `You cannot exchange less than ${btc_amount} BTC.`;
             validationMessages.appendChild(error);
         }
         XMRcurrencyInput.value = XMRtoReceive.toFixed(12);
