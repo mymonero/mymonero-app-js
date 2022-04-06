d3aa4261a7cc86dd6f6e9ab14f56af206c1081c1
Devin <devin@blackhat.co.za>
2022-04-06 13:22:55 +0200

    fix:updated net-utils and updated for custom urls

--- local_modules/MoneroUtils/__IPCSafe_remote_monero_utils.electron.js
+++ local_modules/MoneroUtils/__IPCSafe_remote_monero_utils.electron.js
@@ -6,16 +6,31 @@ const ENVIRONMENT_IS_WEB = typeof window === 'object'
 const ENVIRONMENT_IS_WORKER = typeof importScripts === 'function'
 const ENVIRONMENT_IS_NODE = typeof process === 'object' && process.browser !== true && typeof require === 'function' && ENVIRONMENT_IS_WORKER == false // we want this to be true for Electron but not for a WebView
 if (!ENVIRONMENT_IS_NODE || ENVIRONMENT_IS_WEB) {
-  throw 'Not expecting this module to be included in this environment: non-node or web'
+  throw Error('Not expecting this module to be included in this environment: non-node or web')
 }
 let coreBridge_instance = null
 const local_fns = {}
-const fn_names = require('./__bridged_fns_spec.electron').bridgedFn_names
+const fn_names = [
+  'is_subaddress',
+  'is_integrated_address',
+  'new_payment_id',
+  'new__int_addr_from_addr_and_short_pid',
+  'decode_address',
+  'newly_created_wallet',
+  'are_equal_mnemonics',
+  'mnemonic_from_seed',
+  'seed_and_keys_from_mnemonic',
+  'validate_components_for_login',
+  'address_and_keys_from_seed',
+  'generate_key_image',
+  'estimated_tx_network_fee'
+  // "async__send_funds", // this is not to be bridged via synch IPC since it requires async bridging
+]
 for (const i in fn_names) {
   const name = fn_names[i]
   local_fns[name] = function () {
     if (coreBridge_instance === null) {
-      throw 'Expected coreBridge_instance to have been loaded by the time ' + name + ' was called'
+      throw Error('Expected coreBridge_instance to have been loaded by the time ' + name + ' was called')
     }
     let retVals
     try {
@@ -30,100 +45,64 @@ for (const i in fn_names) {
   const { ipcMain } = require('electron')
   const IPCBridge_cb_jump_map = {}
   ipcMain.on('async__send_funds--authenticate--res_cb', function (event, IPC_arg) {
-    const IPCBridge_call_id = IPC_arg.IPCBridge_call_id
-    const did_pass = IPC_arg.did_pass
-    IPCBridge_cb_jump_map['async__send_funds--authenticate--res_cb_jump_cb--' + IPCBridge_call_id](did_pass)
+    IPCBridge_cb_jump_map['async__send_funds--authenticate--res_cb_jump_cb--'](IPC_arg.did_pass)
   })
   ipcMain.on('async__send_funds--get_unspent_outs--res_cb', function (event, IPC_arg) {
-    const IPCBridge_call_id = IPC_arg.IPCBridge_call_id
-    const err_msg = IPC_arg.err_msg
-    const res = IPC_arg.res
-    IPCBridge_cb_jump_map['async__send_funds--get_unspent_outs--res_cb_jump_cb--' + IPCBridge_call_id](err_msg, res)
+    IPCBridge_cb_jump_map['async__send_funds--get_unspent_outs--res_cb_jump_cb--'](IPC_arg.err_msg, IPC_arg.res)
   })
   ipcMain.on('async__send_funds--get_random_outs--res_cb', function (event, IPC_arg) {
-    const IPCBridge_call_id = IPC_arg.IPCBridge_call_id
-    const err_msg = IPC_arg.err_msg
-    const res = IPC_arg.res
-    IPCBridge_cb_jump_map['async__send_funds--get_random_outs--res_cb_jump_cb--' + IPCBridge_call_id](err_msg, res)
+    IPCBridge_cb_jump_map['async__send_funds--get_random_outs--res_cb_jump_cb--'](IPC_arg.err_msg, IPC_arg.res)
   })
   ipcMain.on('async__send_funds--submit_raw_tx--res_cb', function (event, IPC_arg) {
-    const IPCBridge_call_id = IPC_arg.IPCBridge_call_id
-    const err_msg = IPC_arg.err_msg
-    const res = IPC_arg.res
-    IPCBridge_cb_jump_map['async__send_funds--submit_raw_tx--res_cb_jump_cb--' + IPCBridge_call_id](err_msg, res)
+    IPCBridge_cb_jump_map['async__send_funds--submit_raw_tx--res_cb_jump_cb--'](IPC_arg.err_msg, IPC_arg.res)
   })
   ipcMain.on('async__send_funds', function (event, IPC_arg) {
     if (coreBridge_instance === null) {
-      throw 'Expected coreBridge_instance to have been loaded by the time async__send_funds was called'
+      throw Error('Expected coreBridge_instance to have been loaded by the time async__send_funds was called')
     }
-    const IPCBridge_call_id = IPC_arg.IPCBridge_call_id
     const call_args = IPC_arg.args
     call_args.authenticate_fn = function (cb) {
-      IPCBridge_cb_jump_map['async__send_funds--authenticate--res_cb_jump_cb--' + IPCBridge_call_id] = function (did_pass) {
+      IPCBridge_cb_jump_map['async__send_funds--authenticate--res_cb_jump_cb--'] = function (did_pass) {
         cb(did_pass)
       }
-      event.sender.send('async__send_funds--authenticate_fn', {
-        call_id: IPCBridge_call_id
-      })
+      event.sender.send('async__send_funds--authenticate_fn', {})
       // wait for return ipcMain.on with the IPCBridge_call_id
     }
     call_args.get_unspent_outs_fn = function (req_params, cb) {
-      IPCBridge_cb_jump_map['async__send_funds--get_unspent_outs--res_cb_jump_cb--' + IPCBridge_call_id] = function (err_msg, res) {
+      IPCBridge_cb_jump_map['async__send_funds--get_unspent_outs--res_cb_jump_cb--'] = function (err_msg, res) {
         cb(err_msg, res)
       }
-      event.sender.send('async__send_funds--get_unspent_outs_fn', {
-        call_id: IPCBridge_call_id,
-        req_params: req_params
-      })
+      event.sender.send('async__send_funds--get_unspent_outs_fn', { req_params: req_params })
       // wait for return ipcMain.on with the IPCBridge_call_id
     }
     call_args.get_random_outs_fn = function (req_params, cb) {
-      IPCBridge_cb_jump_map['async__send_funds--get_random_outs--res_cb_jump_cb--' + IPCBridge_call_id] = function (err_msg, res) {
+      IPCBridge_cb_jump_map['async__send_funds--get_random_outs--res_cb_jump_cb--'] = function (err_msg, res) {
         cb(err_msg, res)
       }
-      event.sender.send('async__send_funds--get_random_outs_fn', {
-        call_id: IPCBridge_call_id,
-        req_params: req_params
-      })
+      event.sender.send('async__send_funds--get_random_outs_fn', { req_params: req_params })
       // wait for return ipcMain.on with the IPCBridge_call_id
     }
     call_args.submit_raw_tx_fn = function (req_params, cb) {
-      IPCBridge_cb_jump_map['async__send_funds--submit_raw_tx--res_cb_jump_cb--' + IPCBridge_call_id] = function (err_msg, res) {
+      IPCBridge_cb_jump_map['async__send_funds--submit_raw_tx--res_cb_jump_cb--'] = function (err_msg, res) {
         cb(err_msg, res)
       }
-      event.sender.send('async__send_funds--submit_raw_tx_fn', {
-        call_id: IPCBridge_call_id,
-        req_params: req_params
-      })
+      event.sender.send('async__send_funds--submit_raw_tx_fn', { req_params: req_params })
       // wait for return ipcMain.on with the IPCBridge_call_id
     }
     call_args.willBeginSending_fn = function () {
-      event.sender.send('async__send_funds--willBeginSending_fn', {
-        call_id: IPCBridge_call_id
-      })
+      event.sender.send('async__send_funds--willBeginSending_fn', {})
     }
     call_args.canceled_fn = function () {
-      event.sender.send('async__send_funds--canceled_fn', {
-        call_id: IPCBridge_call_id
-      })
+      event.sender.send('async__send_funds--canceled_fn', {})
     }
     call_args.status_update_fn = function (params) {
-      event.sender.send('async__send_funds--status_update_fn', {
-        call_id: IPCBridge_call_id,
-        params: params
-      })
+      event.sender.send('async__send_funds--status_update_fn', { params: params })
     }
     call_args.success_fn = function (params) {
-      event.sender.send('async__send_funds--success_fn', {
-        call_id: IPCBridge_call_id,
-        params: params
-      })
+      event.sender.send('async__send_funds--success_fn', { params: params })
     }
     call_args.error_fn = function (params) {
-      event.sender.send('async__send_funds--error_fn', {
-        call_id: IPCBridge_call_id,
-        params: params
-      })
+      event.sender.send('async__send_funds--error_fn', { params: params })
     }
     coreBridge_instance.async__send_funds(call_args)
   })
